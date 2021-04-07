abbffefb9a8f7767559ff80b643f53b11621876f
dominikpeter <dominik.peter@gmail.com>
2021-04-07 11:17:30 +0200

    using yml config file

--- instapy/app/simple_bot.py
+++ instapy/app/simple_bot.py
@@ -5,52 +5,39 @@ import time
 
 from instapy import InstaPy
 from instapy import smart_run
+from yml_parser import parse_yml
 
 
 INSTA_USER = os.getenv("INSTA_USER")
 INSTA_PW = os.getenv("INSTA_PW")
 
 
-dont_like = ["nude", "girl", "hot", "sex"]
-friend_list = ["amytolja", "vireaksoeur"]
-likes = ["sunrise", "sunset", "drone",
-         "droneshot", "dronephotography", "rigi",
-         "hallwilersee", "sempach", "standuppaddle",
-         "mountainbike", "switzerland", "sonnenaufgabg",
-         "sonnenuntergang", "drohne", "see", "aargau",
-         "sempachersee", "water", "calm", "relax", "luzern",
-         "bern", "aarau", "wandern", "berge", "droneoftheday", 
-         "nature", "landscape", "natur", "wanderlust", "#lakelife"]
+yml_config = parse_yml("./app/config.yml")
 
 
-i = 1
-while 1==1:
+dont_like = yml_config["dont_like"]
+friend_list = yml_config["friends"]
+likes = yml_config["likes"]
+max_followers = yml_config["max_followers"]
+min_followers = yml_config["min_fallowers"]
+min_following = yml_config["min_following"]
+n_likes = yml_config["n_likes"]
 
-    print(f"Starting run {i}")
 
-    session = InstaPy(username=INSTA_USER,
-                password=INSTA_PW,
-                headless_browser=True)
+session = InstaPy(username=INSTA_USER,
+                  password=INSTA_PW,
+                  headless_browser=True)
 
-    with smart_run(session):
+with smart_run(session):
 
-        """ Activity flow """
-        # general settings
-        session.set_relationship_bounds(enabled=True,
-                                        delimit_by_numbers=True,
-                                        max_followers=45900,
-                                        min_followers=20,
-                                        min_following=20)
+    """ Activity flow """
+    # general settings
+    session.set_relationship_bounds(enabled=True,
+                                    delimit_by_numbers=True,
+                                    max_followers=max_followers,
+                                    min_followers=min_followers,
+                                    min_following=min_following)
 
-        session.set_dont_include(friend_list)
-        session.set_dont_like(dont_like)
-        session.like_by_tags(likes, amount=100)
-
-    session.stop()
-
-    i += 1
-
-    sleep_time = random.randint(1800,14400)
-    print(f"Sleeping for {sleep_time} seconds")
-
-    time.sleep(sleep_time)
+    session.set_dont_include(friend_list)
+    session.set_dont_like(dont_like)
+    session.like_by_tags(likes, amount=n_likes)
---
+++ instapy/app/yml_parser.py
@@ -0,0 +1,10 @@
+
+import yaml
+
+def parse_yml(path):
+    with open(path, 'r') as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            return None
